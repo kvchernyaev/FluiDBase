6567511b05cef98c856cf35566b6d44965c3b6d3
cherniaevkv <cherniaevkv@suek.ru>
2017-12-26 19:16:26 +0300

    just create project

---
+++ FluiDBase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FluiDBase
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
