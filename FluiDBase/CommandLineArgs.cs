c3a15c966513da3dc384286967b550442416e8a1
cherniaevkv <cherniaevkv@suek.ru>
2017-12-29 14:39:28 +0300

    reference commandlineparser

---
+++ FluiDBase/CommandLineArgs.cs
@@ -0,0 +1,32 @@
+﻿using CommandLineParser.Arguments;
+
+
+namespace FluiDBase
+{
+    public class CommandLineArgs
+    {
+        [SwitchArgument('s', "show", true, Description = "Set whether show or not")]
+        public bool show;
+
+        private bool hide;
+        [SwitchArgument('h', "hide", false, Description = "Set whether hid or not")]
+        public bool Hide
+        {
+            get { return hide; }
+            set { hide = value; }
+        }
+
+        [ValueArgument(typeof(decimal), 'v', "version", Description = "Set desired version")]
+        public decimal version;
+
+        [ValueArgument(typeof(string), 'l', "level", Description = "Set the level")]
+        public string level;
+
+        [BoundedValueArgument(typeof(int), 'o', "optimization", 
+            MinValue = 0, MaxValue = 3, Description = "Level of optimization")]
+        public int optimization;
+
+        [EnumeratedValueArgument(typeof(string),'c', "color", AllowedValues = "red;green;blue")]
+        public string color;
+    }
+}
--- FluiDBase/Program.cs
+++ FluiDBase/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
 
 
 namespace FluiDBase
@@ -8,6 +10,32 @@ namespace FluiDBase
     {
         public static void Main(string[] args)
         {
+            // https://github.com/j-maly/CommandLineParser
+            CommandLineParser.CommandLineParser parser = 
+                new CommandLineParser.CommandLineParser();
+            
+            
+            SwitchArgument showArgument = new SwitchArgument(
+                's', "show", "Set whether show or not", true);
+            ValueArgument<decimal> version = new ValueArgument<decimal> (
+                'e', "version", "Set desired version");
+            EnumeratedValueArgument<string> color = new EnumeratedValueArgument<string> (
+                'c', "color", new string[] { "red", "green", "blue" });
+
+            parser.Arguments.Add(showArgument);
+            parser.Arguments.Add(version);
+            parser.Arguments.Add(color);
+            
+            try 
+            {
+                parser.ParseCommandLine(args); 
+                parser.ShowParsedArguments();
+            }
+            catch (CommandLineException e)
+            {
+                Console.WriteLine("CommandLineException");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
